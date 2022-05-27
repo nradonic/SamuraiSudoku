f73ed2ef9391a12be9c1634375f7de875f779303
Nick Radonic <975247+nradonic@users.noreply.github.com>
2022-05-26 22:59:16 -0400

    updated for GitHub posting - with selection of test file or user file through file system selector

--- src/main/java/sudoku/Start.java
+++ src/main/java/sudoku/Start.java
@@ -9,8 +9,14 @@ public class Start
 {
     public static void main(String[] args)
     {
+//  Either from a fixed resource file or from an external JSON text file. See the resources wapo file for necessary format
+        // blockPositions represent 9x9 grids that are overlapping and must be internally consistent to be solved
 
+        // test file
         ExpandedGrid expandedGrid = ExpandedGridIO.loadBoardFromResources("wapo_5_1_21x21.sudoku");
+
+        // select a file from the file system
+        // ExpandedGrid expandedGrid = ExpandedGridIO.loadExpandedGrid();
 
         // map expanded grid to a grid of Sudoku cells
         SudokuCells sudokuGrid = new SudokuCells(expandedGrid);
